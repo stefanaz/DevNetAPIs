aed6a3a24e5d99764a3712e3aea95a2a4d8676f6
NStefaniuk <stefan2907@gmail.com>
2020-07-14 16:21:28 +0200

    Users & Auth:
    -- general structure of routes
    -- created user model 
    -- created MongoDB connections 
    -- set up JWT token
    -- route create user
    -- route auth user
    -- config setup
    -- route custom middleware connected

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect DB:
+connectDB();
+
+// Init Middleware (to use req.body in routes)
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API running"));
+
+// Define Routes:
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
